ed6512738def2e82c3ee9ccc7e0a28f8d8053815
Mskeen <s29920@pjwstk.edu.pl>
2026-03-24 09:32:51 +0100

    Add User model and UserType enum

---
+++ UniversityRentalApp/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityRentalApp.Models
+{
+    public enum UserType{
+    Student=1,
+    Employee=2
+    }
+
+    public class User { 
+    public Guid Id { get; private set; }
+    public String FirstName {  get; private set; }
+    public string LastName { get; private set; }
+    public UserType Type { get; private set; }
+
+        public User(String firstName, String lastName, UserType type) { 
+        Id=Guid.NewGuid();
+        FirstName=firstName;
+        LastName=lastName;
+        Type=type;
+       
+        }
+        public override string ToString()
+        {
+            return "{FirstName} {LastName} ({Type}) - ID: {Id}";
+        }
+
+    }
+
+}
